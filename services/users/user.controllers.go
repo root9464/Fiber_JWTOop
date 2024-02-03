178ecb706dd0c052f1fdeea2011aa49599691e81
root9464 <senpai9464@yandex.ru>
2024-02-04 03:20:42 +0500

    04.02.2024 | 3:20 || "Add/Get/Delete"

--- services/users/user.controllers.go
+++ services/users/user.controllers.go
@@ -7,4 +7,5 @@ func AddUserRoutes(routes fiber.Router, gnu UserService) {
     routes.Get("/users/hello", gnu.HelloUser)
     routes.Post("/users/adduser", gnu.AddUser)
     routes.Get("/users/allusers", gnu.GetAllUsers)
+    routes.Delete("/users/:id", gnu.GetUserByID)
 }
--- services/users/users.service.go
+++ services/users/users.service.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"root/services/consts"
+
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -8,6 +10,7 @@ import (
 type UserService interface {
     AddUser(context *fiber.Ctx) error
     GetAllUsers(context *fiber.Ctx) error
+    GetUserByID(context *fiber.Ctx) error
     HelloUser(context *fiber.Ctx) error
 }
 
@@ -18,14 +21,14 @@ type DataUserService struct {
 func (method *DataUserService) AddUser(c *fiber.Ctx) error {
     user := new(User)
     if err := c.BodyParser(user); err != nil {
-        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+        return c.Status(fiber.StatusBadRequest).SendString(consts.BadReuest + err.Error())
     }
     if method.DB == nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Database connection is nil"})
+		return c.Status(fiber.StatusInternalServerError).SendString(consts.ErrorConnectToDb +  method.DB.Error.Error())
     }
     result := method.DB.Create(&user)
     if result.Error != nil {
-        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": result.Error.Error()})
+        return c.Status(fiber.StatusInternalServerError).SendString(consts.ErrorQueryDb + result.Error.Error())
     }
 
     return c.Status(fiber.StatusOK).JSON(user)
@@ -38,13 +41,30 @@ func (method *DataUserService) HelloUser(c *fiber.Ctx) error {
 func (method *DataUserService) GetAllUsers(c *fiber.Ctx) error {
     var users []User
     if method.DB == nil {
-        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Database connection is nil"})
+        return c.Status(fiber.StatusInternalServerError).SendString(consts.ErrorNil +  method.DB.Error.Error())
     }
     result := method.DB.Find(&users)
     if result.Error != nil {
-        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": result.Error.Error()})
+        return c.Status(fiber.StatusInternalServerError).SendString(consts.ErrorQueryDb + result.Error.Error())
     }
     return c.Status(fiber.StatusOK).JSON(users)
+}
+
+func (method *DataUserService) GetUserByID(c *fiber.Ctx) error {
+    id := c.Params("id")
+    var user User
+	result := method.DB.Where("id = ?", id).Delete(&user)
+
+	if id == "" {
+		return c.Status(400).SendString(consts.ErrorNil)
+	}
+    if result.Error != nil {
+        if result.Error == gorm.ErrRecordNotFound {
+            return c.Status(404).SendString(consts.ErrorNil)
+        }
+        return c.Status(500).SendString(consts.ErrorQueryDb + result.Error.Error())
+    }
+	return c.Status(200).SendString("Пользователь успешно удален")
 }
 
 func MethodUserService(db *gorm.DB) UserService {
